4d6368baef34c7ecd335b3615fe05fc0219b50e0
june <khm11904@naver.com>
2021-03-15 22:28:33 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ app.use(
     secret: process.env.COOKIE_SECRET,
     resave: true,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: process.env.MONGO_URL }),
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URL_PROD }),
   })
 );
 app.use(passport.initialize());
